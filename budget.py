5167de0db3a5e9681dc15d97939e87801528249b
jayvier <74190201+jayvier@users.noreply.github.com>
2022-06-30 18:45:29 +0200

    Update budget.py

--- budget.py
+++ budget.py
@@ -8,9 +8,28 @@ class Category:
     The deposit function will take an amount 
     description of function
     """
-    self.ledger.append({"amount": -amount, "description": description})
+    self.ledger.append({"amount": amount, "description": description})
 
   def withdraw(self, amount, description=""):
-    return None
+    if self.check_funds(amount):
+      self.ledger.append({"amount": -amount, "description": description})
+    else:
+      print("Insufficient funds")
 
-  def chech_funds(self, amount):
+  
+  def check_funds(self, amount):
+    if amount > self.get_balance():
+      return False
+    else:
+      return True
+
+  def get_balance(self):
+    total = 0
+    for trans in self.ledger:
+      total += trans["amount"]
+    return total
+
+  def transfer(self, amount, category):
+    if self.check_funds(amount):
+      self.withdraw(amount, "Transfer to " + category.name)
+      category.deposit(amount, "Transfer from " + category.name)
